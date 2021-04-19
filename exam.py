98de4d64d32d3b5d717f4153600e68609bb7dab0
Sarim-Qureshi <sarim.asq@gmail.com>
2021-04-19 13:26:13 +0530

    Add home.py, student.py, exam.py
    home.py is the home page where user will choose his/her role and proceed
    to their respective pages. student.py and exam.py are like dashboard of
    student and exam cell respectively.

---
+++ exam.py
@@ -0,0 +1,74 @@
+from tkinter import Tk, Label, Button, Frame
+
+
+def update_marks():
+    pass
+
+
+def update_timetables():
+    pass
+
+
+def check_university_updates():
+    pass
+
+
+def post_results():
+    pass
+
+
+def on_enter(e):
+    e.widget['background'] = '#051094'
+    e.widget['foreground'] = 'white'
+
+
+def on_leave(e):
+    e.widget['background'] = '#0277bd'
+    e.widget['foreground'] = 'black'
+
+
+root = Tk()
+root.geometry("1000x600+0+0")
+root.resizable(False, False)
+root.configure(background='#222')
+head = Label(root, text='Student Information Management System', font='consolas 30 bold')
+head.pack(pady=(35, 0))
+head.configure(background='#222', foreground='white')
+
+f1 = Frame(root, width=300, height=70, borderwidth=10, relief='groove')
+f1.configure(background='#999')
+f1.pack(pady=(50, 20), anchor='c')
+b1 = Button(f1, text='Update marks', command=update_marks, cursor='hand2', font='consolas 14 bold', pady=1, padx=10)
+b1.bind("<Enter>", on_enter)
+b1.bind("<Leave>", on_leave)
+b1.pack()
+b1.configure(background='#0277bd')
+
+f2 = Frame(root, width=300, height=70, borderwidth=10, relief='groove')
+f2.configure(background='#999')
+f2.pack(pady=(20, 20), anchor='c')
+b2 = Button(f2, text='Update timetables', command=update_timetables, cursor='hand2', font='consolas 14 bold', pady=1, padx=10)
+b2.bind("<Enter>", on_enter)
+b2.bind("<Leave>", on_leave)
+b2.pack()
+b2.configure(background='#0277bd')
+
+f3 = Frame(root, width=300, height=70, borderwidth=10, relief='groove')
+f3.configure(background='#999')
+f3.pack(pady=(20, 20), anchor='c')
+b3 = Button(f3, text='Check university updates', command=check_university_updates, cursor='hand2', font='consolas 14 bold', pady=1, padx=10)
+b3.bind("<Enter>", on_enter)
+b3.bind("<Leave>", on_leave)
+b3.pack()
+b3.configure(background='#0277bd')
+
+f4 = Frame(root, width=300, height=70, borderwidth=10, relief='groove')
+f4.configure(background='#999')
+f4.pack(pady=(20, 20), anchor='c')
+b4 = Button(f4, text='Post results', command=post_results, cursor='hand2', font='consolas 14 bold', pady=1, padx=10)
+b4.bind("<Enter>", on_enter)
+b4.bind("<Leave>", on_leave)
+b4.pack()
+b4.configure(background='#0277bd')
+
+root.mainloop()
---
+++ home.py
@@ -0,0 +1,66 @@
+from tkinter import Tk, Label, Radiobutton, StringVar, Button, messagebox, Frame
+import os
+
+
+def handle_radio():
+    if var.get() == 'NA':
+        messagebox.showinfo('Choose your role', 'You have not specified your role')
+    else:
+        if var.get() == 'A':
+            os.system('student.py')
+        elif var.get() == 'C':
+            os.system('exam.py')
+        print(var.get())
+
+
+def on_enter(e):
+    b['background'] = '#033500'
+    b['foreground'] = 'white'
+
+
+def on_leave(e):
+    b['background'] = '#3cb043'
+    b['foreground'] = 'black'
+
+
+root = Tk()
+root.geometry("1250x650+0+0")
+root.resizable(False, False)
+root.configure(background='#222')
+
+head = Label(root, text='Student Information Management System', font='consolas 30 bold')
+head.pack(pady=(35, 0))
+head.configure(background='#222', foreground='white')
+
+f = Frame(root, width=300, height=100, borderwidth=30, relief='groove')
+f.configure(background='#999')
+f.pack(pady=50)
+
+l = Label(f, text='Choose your role', font='consolas 22 bold', padx=15)
+l.pack(pady=(20, 15))
+l.configure(background='#999', foreground='#111')
+var = StringVar()
+var.set('NA')
+r1 = Radiobutton(f, text='Student', variable=var, value='A', padx=30, font='consolas 14 bold')
+r1.pack(pady=5, anchor='w')
+r1.configure(background='#999', foreground='#111')
+r2 = Radiobutton(f, text='Faculty', variable=var, value='B', padx=30, font='consolas 14 bold')
+r2.pack(pady=5, anchor='w')
+r2.configure(background='#999', foreground='#111')
+r3 = Radiobutton(f, text='Exam section', variable=var, value='C', padx=30, font='consolas 14 bold')
+r3.pack(pady=5, anchor='w')
+r3.configure(background='#999', foreground='#111')
+r4 = Radiobutton(f, text='Placement Cell', variable=var, value='D', padx=30, font='consolas 14 bold')
+r4.pack(pady=5, anchor='w')
+r4.configure(background='#999', foreground='#111')
+r5 = Radiobutton(f, text='Administrator', variable=var, value='E', padx=30, font='consolas 14 bold')
+r5.pack(pady=5, anchor='w')
+r5.configure(background='#999', foreground='#111')
+
+b = Button(f, text='Next', command=handle_radio, cursor='hand2', font='consolas 14 bold', pady=1, padx=10)
+b.bind("<Enter>", on_enter)
+b.bind("<Leave>", on_leave)
+b.pack(pady=(10, 20))
+b.configure(background='#3cb043')
+
+root.mainloop()
